bdeb5940296e8a7be665d950c41bc6939f9289a3
Abhishek <117830155+abhithemachinegun@users.noreply.github.com>
2026-03-16 10:24:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
                 current += 1
                
     async def main():
-        await VJBot.startu()
+        await VJBot.start()
         bot_info  = await VJBot.get_me()
         await restart_forwards(VJBot)
         print("Bot Started.")
